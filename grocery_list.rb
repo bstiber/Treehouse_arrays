c05679638434d9627319e6a2ce57eeaf576aa66d
Bill Stiber <billstiber@gmail.com>
2016-05-29 17:59:05 -0400

    add file grocery_list

---
+++ grocery_list.rb
@@ -0,0 +1,14 @@
+# Create a method named "create_shopping_list" that returns a hash. It does not need to ask
+# for a name or get anything from standard input.
+
+# creates a new array, not hash
+def create_shopping_list
+  array = { 'title' => 'Grocery List', 'items' => Array.new }
+  return hash
+end
+
+# creates a new hash called hash
+def create_shopping_list
+  hash = { 'title' => 'Grocery List', 'items' => Hash.new }
+  return hash
+end
