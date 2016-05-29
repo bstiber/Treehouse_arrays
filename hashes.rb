e400d20e5689b3be6118827abdfbae9f72a98d91
Bill Stiber <billstiber@gmail.com>
2016-05-28 21:45:06 -0400

    add more hash practice

--- hashes.rb
+++ hashes.rb
@@ -6,3 +6,13 @@ list["friends"] = "jessica"
 
 # treehouse challenge
 grocery_item_1 = {"name" => "milk"}
+
+# Hash keys
+hash = { "name" => "Bread", "quantity" => 1, "calories" => 100 }
+
+# using has_key? (key?) method, check if the has variable has a key called
+# "calories".  If it does, set a new variable called "food" to true.
+
+hash = { "name" => "Bread", "quantity" => 1, "calories" => 100 }
+hash.has_key?("calories")
+food = true
