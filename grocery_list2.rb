1f7a28991a8d61eff1289c9d05b2d34f9204360e
Bill Stiber <billstiber@gmail.com>
2016-05-29 19:16:27 -0400

    add more code to list

--- grocery_list2.rb
+++ grocery_list2.rb
@@ -1,8 +1,18 @@
 # shopping list follow along with video
 
 def create_list
-  print "what is the list name?"
+  print "what is the list name? "
   name = gets.chomp
-  hash = {"name" => name, "items" => Array.new}
-  return Hash
+  hash = { "name" => name, "items" => Array.new}
+  return hash
 end
+
+def add_list_item
+  print "what is your item? "
+  item_name = gets.chomp
+  hash = {"name" => name}
+  return hash
+end
+
+list = create_list()
+puts list.inspect
